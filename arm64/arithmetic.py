00d79c32c76ae8177214c158c6c2779f5f1d45b8
Mark Brand <markbrand@google.com>
2016-02-26 14:45:11 +0100

    Fixing dates in file creation headers

--- arm64/arithmetic.py
+++ arm64/arithmetic.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#    Copyright 2014 Mark Brand - c01db33f (at) gmail.com
+#    Copyright 2016 Mark Brand - c01db33f (at) gmail.com
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
